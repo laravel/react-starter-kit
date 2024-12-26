1a4c5e477fee61031fc8aaecab77f6e24e1a79ed
Taylor Otwell <taylor@laravel.com>
2024-12-26 16:40:46 -0600

    comma

--- resources/js/Pages/Auth/Register.tsx
+++ resources/js/Pages/Auth/Register.tsx
@@ -40,7 +40,7 @@ export default function Register() {
     return (
         <AuthLayout 
             title="Create an account"
-            description="Enter your name, email and password below."
+            description="Enter your name, email, and password below."
         >
             <Head title="Register" />
             <form className="flex flex-col gap-6" onSubmit={submit}>
