0445234f03c922932a041102886dafb06ae5b425
Tony Lea <tony@devdojo.com>
2025-01-02 17:47:00 -0500

    renaming files/folders

---
+++ resources/js/components/heading.tsx
@@ -0,0 +1,22 @@
+import { Separator } from "@/components/ui/separator"
+
+interface HeadingProps {
+    title: string;
+    description?: string;
+}
+
+export default function Heading({ title, description }: HeadingProps) {
+    return (
+        <>
+            <div className="space-y-0.5">
+                <h2 className="text-lg sm:text-xl md:text-2xl font-bold tracking-tight">{title}</h2>
+                {description && (
+                    <p className="text-sm md:text-base text-muted-foreground">
+                        {description}
+                    </p>
+                )}
+            </div>
+            <Separator className="my-6" />
+        </>
+    );
+}
---
+++ resources/js/pages/settings/appearance.tsx
@@ -0,0 +1,45 @@
+import { FormEventHandler } from 'react'
+import { Head, Link, useForm, usePage } from '@inertiajs/react'
+
+import AppearanceTabs from '@/components/appearance-tabs';
+import SettingsHeading from "@/components/settings/heading";
+import { Label } from "@/components/ui/label";
+
+import AppLayout from '@/layouts/app-layout'
+import SettingsLayout from './layout'
+
+interface BreadcrumbItem {
+    title: string
+    href: string
+}
+
+const breadcrumbItems: BreadcrumbItem[] = [
+    {
+        title: 'Appearance Settings',
+        href: '/settings/appearance'
+    }
+]
+
+export default function Appearance() {
+
+    return (
+        <AppLayout
+            breadcrumbItems={breadcrumbItems}
+        >
+            <Head title="Appearance Settings" />
+
+            <SettingsLayout>   
+                <div>
+                    <SettingsHeading 
+                        title="Appearance Settings"
+                        description="Update your account's appearance settings"
+                    />
+                    <div className="flex flex-col gap-4 items-start">
+                        <Label>Appearance</Label>
+                        <AppearanceTabs />
+                    </div>
+                </div>
+            </SettingsLayout>
+        </AppLayout>
+    );
+}
