e23052f58803c62f0fc64f4cc42087585ad39b26
Tony Lea <tony@devdojo.com>
2025-01-10 12:45:23 -0500

    Adding ziggy global definition

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,7 +3,12 @@ import '../css/app.css';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import { route as routeFn } from 'ziggy-js';
 import { initializeTheme } from './hooks/use-appearance';
+
+declare global {
+    var route: typeof routeFn;
+}
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
