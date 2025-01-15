998ceb30ca3d44f8a7fb1393311f49fa21173903
Tony Lea <tony@devdojo.com>
2025-01-15 15:25:22 -0500

    fixing the forgot password import

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -2,19 +2,16 @@ import { AppHeader } from '@/components/app-header';
 import { AppShell } from '@/components/app-shell';
 import { AppSidebar } from '@/components/app-sidebar';
 import { type BreadcrumbItem } from '@/types';
-
-// For the sidebar inset variant, uncomment the import line below and replace <main> with <SidebarInset>
-// If you are not using the sidebar inset, remove these comments and keep the default layout.
-//import { SidebarInset } from '@/components/ui/sidebar';
+import { SidebarInset } from '@/components/ui/sidebar';
 
 export default function App({ children, breadcrumbs = [] }: { children: React.ReactNode; breadcrumbs?: BreadcrumbItem[] }) {
     return (
         <AppShell>
             <AppSidebar />
-            <main>
+            <SidebarInset>
                 <AppHeader breadcrumbs={breadcrumbs} />
                 {children}
-            </main>
+            </SidebarInset>
         </AppShell>
     );
 }
