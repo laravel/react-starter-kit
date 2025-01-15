9539d99ebb2b445492a24e13d3a37b5adbc27d6f
Tony Lea <tony@devdojo.com>
2025-01-15 11:28:04 -0500

    Adding updated layout comments

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,9 +1,11 @@
 import { AppHeader } from '@/components/app-header';
 import { AppShell } from '@/components/app-shell';
 import { AppSidebar } from '@/components/app-sidebar';
-// If you want to use the sidebar inset, uncomment the following line and replace <main> with <SidebarInset>
-//import { SidebarInset } from '@/components/ui/sidebar';
 import { type BreadcrumbItem } from '@/types';
+
+// For the sidebar inset variant, uncomment the import line below and replace <main> with <SidebarInset>
+// If you are not using the sidebar inset, remove these comments and keep the default layout.
+//import { SidebarInset } from '@/components/ui/sidebar';
 
 export default function App({ children, breadcrumbs = [] }: { children: React.ReactNode; breadcrumbs?: BreadcrumbItem[] }) {
     return (
