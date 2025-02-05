3ec1eb4570ae69d358e776f37c454bbc8d58485c
Ryuta Hamasaki <avosalmon@gmail.com>
2025-02-05 14:01:12 +0900

    Format

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -1,42 +1,25 @@
-import { Link, usePage } from '@inertiajs/react';
-import { type BreadcrumbItem } from '@/types';
-import AppLogo from './app-logo';
-import AppLogoIcon from './app-logo-icon';
-import { Button } from "@/components/ui/button"
 import { Breadcrumbs } from '@/components/breadcrumbs';
+import { Icon } from '@/components/icon';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import {
     NavigationMenu,
     NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
     navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from "@/components/ui/tooltip"
-import { type NavItem } from '@/types';
-import {
-    Sheet,
-    SheetContent,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-} from "@/components/ui/sheet"
-import { type SharedData } from '@/types';
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Menu, ChevronDown, FolderGit2, BookOpenText, Search, LayoutGrid } from 'lucide-react'
-import { useInitials } from '@/hooks/use-initials';
-import { cn } from "@/lib/utils";
+} from '@/components/ui/navigation-menu';
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { UserMenuContent } from '@/components/user-menu-content';
-import { Icon } from '@/components/icon';
+import { useInitials } from '@/hooks/use-initials';
+import { cn } from '@/lib/utils';
+import { type BreadcrumbItem, type NavItem, type SharedData } from '@/types';
+import { Link, usePage } from '@inertiajs/react';
+import { BookOpenText, ChevronDown, FolderGit2, LayoutGrid, Menu, Search } from 'lucide-react';
+import AppLogo from './app-logo';
+import AppLogoIcon from './app-logo-icon';
 
 const mainNavItems: NavItem[] = [
     {
@@ -59,7 +42,7 @@ const rightNavItems: NavItem[] = [
     },
 ];
 
-const activeItemStyles = "bg-neutral-100 text-neutral-900 dark:bg-neutral-800 dark:text-neutral-100";
+const activeItemStyles = 'bg-neutral-100 text-neutral-900 dark:bg-neutral-800 dark:text-neutral-100';
 
 interface AppHeaderProps {
     breadcrumbs?: BreadcrumbItem[];
@@ -70,125 +53,128 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
     const getInitials = useInitials();
     return (
         <>
-        <div className="border-b border-sidebar-border/80 ">
-            <div className="flex h-16 items-center px-4  md:max-w-7xl mx-auto">
-                {/* Mobile Menu */}
-                <div className="lg:hidden">
-                    <Sheet>
-                        <SheetTrigger asChild>
-                            <Button variant="ghost" size="icon" className="mr-2 w-[34px] h-[34px]">
-                                <Menu className="h-5 w-5" />
-                            </Button>
-                        </SheetTrigger>
-                        <SheetContent side="left" className="bg-neutral-50 h-full flex flex-col items-stretch justify-between w-64">
-                            <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
-                            <SheetHeader className="text-left flex justify-start">
-                                <AppLogoIcon className="h-6 w-6 fill-current text-black dark:text-white" />
-                            </SheetHeader>
-                            <div className="flex flex-col h-full space-y-4 mt-6 flex-1">
-                                <div className="flex flex-col text-sm h-full justify-between">
-                                    <div className="flex flex-col space-y-4">
-                                        {mainNavItems.map((item) => (
-                                            <Link key={item.title} href={item.url} className="flex items-center space-x-2 font-medium">
-                                                {item.icon && <Icon iconNode={item.icon} className="h-5 w-5" />}
-                                                <span>{item.title}</span>
-                                            </Link>
-                                        ))}
-                                    </div>
+            <div className="border-b border-sidebar-border/80">
+                <div className="mx-auto flex h-16 items-center px-4 md:max-w-7xl">
+                    {/* Mobile Menu */}
+                    <div className="lg:hidden">
+                        <Sheet>
+                            <SheetTrigger asChild>
+                                <Button variant="ghost" size="icon" className="mr-2 h-[34px] w-[34px]">
+                                    <Menu className="h-5 w-5" />
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent side="left" className="flex h-full w-64 flex-col items-stretch justify-between bg-neutral-50">
+                                <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
+                                <SheetHeader className="flex justify-start text-left">
+                                    <AppLogoIcon className="h-6 w-6 fill-current text-black dark:text-white" />
+                                </SheetHeader>
+                                <div className="mt-6 flex h-full flex-1 flex-col space-y-4">
+                                    <div className="flex h-full flex-col justify-between text-sm">
+                                        <div className="flex flex-col space-y-4">
+                                            {mainNavItems.map((item) => (
+                                                <Link key={item.title} href={item.url} className="flex items-center space-x-2 font-medium">
+                                                    {item.icon && <Icon iconNode={item.icon} className="h-5 w-5" />}
+                                                    <span>{item.title}</span>
+                                                </Link>
+                                            ))}
+                                        </div>
 
-                                    <div className="flex flex-col space-y-4">
-                                        {rightNavItems.map((item) => (
-                                            <a key={item.title} href={item.url} target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 font-medium">
-                                                {item.icon && <Icon iconNode={item.icon} className="h-5 w-5" />}
-                                                <span>{item.title}</span>
-                                            </a>
-                                        ))}
+                                        <div className="flex flex-col space-y-4">
+                                            {rightNavItems.map((item) => (
+                                                <a
+                                                    key={item.title}
+                                                    href={item.url}
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                    className="flex items-center space-x-2 font-medium"
+                                                >
+                                                    {item.icon && <Icon iconNode={item.icon} className="h-5 w-5" />}
+                                                    <span>{item.title}</span>
+                                                </a>
+                                            ))}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </SheetContent>
-                    </Sheet>
-                </div>
-
-                <Link href="/dashboard" prefetch className="flex items-center space-x-2">
-                    <AppLogo />
-                </Link>
-
-                {/* Desktop Navigation */}
-                <div className="hidden lg:flex items-center space-x-6  h-full ml-6">
-                    <NavigationMenu className="h-full flex items-stretch ">
-                        <NavigationMenuList className="h-full  space-x-2 flex items-stretch">
-
-                            {mainNavItems.map((item, index) => (
-                                <NavigationMenuItem key={index} className="h-full flex items-center relative">
-                                    <Link href={item.url}>
-                                        <NavigationMenuLink className={cn(
-                                            navigationMenuTriggerStyle(),
-                                            activeItemStyles,
-                                            "px-3 h-9 cursor-pointer"
-                                        )}>
-                                            {item.icon && <Icon iconNode={item.icon} className="mr-2 h-4 w-4" />}
-                                            {item.title}
-                                        </NavigationMenuLink>
-                                    </Link>
-                                    <div className="h-0.5 translate-y-px bg-black dark:bg-white w-full absolute bottom-0 left-0"></div>
-                                </NavigationMenuItem>
-                            ))}
-                        </NavigationMenuList>
-                    </NavigationMenu>
-                </div>
-
-                <div className="ml-auto flex items-center space-x-2">
-                    <div className="relative flex items-center space-x-1">
-                        <Button variant="ghost" size="icon" className="w-9 h-9 cursor-pointer">
-                            <Search className="h-5 w-5" />
-                        </Button>
-                        <div className="hidden lg:flex space-x-1">
-                            {rightNavItems.map((item) => (
-                                <TooltipProvider delayDuration={0}>
-                                    <Tooltip>
-                                        <TooltipTrigger>
-                                            <Button key={item.title} variant="ghost" size="icon" asChild className="w-9 h-9 cursor-pointer">
-                                                <a href={item.url} target="_blank" rel="noopener noreferrer">
-                                                    <span className="sr-only">{item.title}</span>
-                                                    {item.icon && <Icon iconNode={item.icon} className="h-5 w-5" />}
-                                                </a>
-                                            </Button>
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            <p>{item.title}</p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider>
-                            ))}
-                        </div>
+                            </SheetContent>
+                        </Sheet>
                     </div>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="ghost" className="h-9 px-1.5">
-                                <Avatar className="h-7 w-7 overflow-hidden rounded-lg">
-                                    <AvatarImage src={auth.user.avatar} alt={auth.user.name} />
-                                    <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
-                                        {getInitials(auth.user.name)}
-                                    </AvatarFallback>
-                                </Avatar>
-                                <ChevronDown className="h-4 w-4 hidden lg:block" />
+
+                    <Link href="/dashboard" prefetch className="flex items-center space-x-2">
+                        <AppLogo />
+                    </Link>
+
+                    {/* Desktop Navigation */}
+                    <div className="ml-6 hidden h-full items-center space-x-6 lg:flex">
+                        <NavigationMenu className="flex h-full items-stretch">
+                            <NavigationMenuList className="flex h-full items-stretch space-x-2">
+                                {mainNavItems.map((item, index) => (
+                                    <NavigationMenuItem key={index} className="relative flex h-full items-center">
+                                        <Link href={item.url}>
+                                            <NavigationMenuLink
+                                                className={cn(navigationMenuTriggerStyle(), activeItemStyles, 'h-9 cursor-pointer px-3')}
+                                            >
+                                                {item.icon && <Icon iconNode={item.icon} className="mr-2 h-4 w-4" />}
+                                                {item.title}
+                                            </NavigationMenuLink>
+                                        </Link>
+                                        <div className="absolute bottom-0 left-0 h-0.5 w-full translate-y-px bg-black dark:bg-white"></div>
+                                    </NavigationMenuItem>
+                                ))}
+                            </NavigationMenuList>
+                        </NavigationMenu>
+                    </div>
+
+                    <div className="ml-auto flex items-center space-x-2">
+                        <div className="relative flex items-center space-x-1">
+                            <Button variant="ghost" size="icon" className="h-9 w-9 cursor-pointer">
+                                <Search className="h-5 w-5" />
                             </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent className="w-56" align="end">
-                            <UserMenuContent user={auth.user} />
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+                            <div className="hidden space-x-1 lg:flex">
+                                {rightNavItems.map((item) => (
+                                    <TooltipProvider delayDuration={0}>
+                                        <Tooltip>
+                                            <TooltipTrigger>
+                                                <Button key={item.title} variant="ghost" size="icon" asChild className="h-9 w-9 cursor-pointer">
+                                                    <a href={item.url} target="_blank" rel="noopener noreferrer">
+                                                        <span className="sr-only">{item.title}</span>
+                                                        {item.icon && <Icon iconNode={item.icon} className="h-5 w-5" />}
+                                                    </a>
+                                                </Button>
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                <p>{item.title}</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                ))}
+                            </div>
+                        </div>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="ghost" className="h-9 px-1.5">
+                                    <Avatar className="h-7 w-7 overflow-hidden rounded-lg">
+                                        <AvatarImage src={auth.user.avatar} alt={auth.user.name} />
+                                        <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                            {getInitials(auth.user.name)}
+                                        </AvatarFallback>
+                                    </Avatar>
+                                    <ChevronDown className="hidden h-4 w-4 lg:block" />
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-56" align="end">
+                                <UserMenuContent user={auth.user} />
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
                 </div>
             </div>
-        </div>
-        {breadcrumbs.length > 1 && (
-            <div className="w-full flex border-b border-sidebar-border/70">
-                <div className="flex h-12 items-center justify-start w-full px-4  md:max-w-7xl mx-auto text-neutral-500">
-                    <Breadcrumbs breadcrumbs={breadcrumbs} />
+            {breadcrumbs.length > 1 && (
+                <div className="flex w-full border-b border-sidebar-border/70">
+                    <div className="mx-auto flex h-12 w-full items-center justify-start px-4 text-neutral-500 md:max-w-7xl">
+                        <Breadcrumbs breadcrumbs={breadcrumbs} />
+                    </div>
                 </div>
-            </div>
-        )}
-    </>
-    )
+            )}
+        </>
+    );
 }
