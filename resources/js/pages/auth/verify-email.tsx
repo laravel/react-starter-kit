5e01767fe62e99fa3cb623ff6e0598e45e7ca24d
Tony Lea <tony@devdojo.com>
2025-02-06 13:59:48 -0500

    Adding updates to the verifed email controller to get tests passing

--- resources/js/pages/auth/verify-email.tsx
+++ resources/js/pages/auth/verify-email.tsx
@@ -16,13 +16,8 @@ export default function VerifyEmail({ status }: { status?: string }) {
     };
 
     return (
-        <AuthLayout>
+        <AuthLayout title="Verify Email" description="Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another">
             <Head title="Email Verification" />
-
-            <div className="mb-4 text-sm text-gray-600">
-                Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If
-                you didn't receive the email, we will gladly send you another
-            </div>
 
             {status === 'verification-link-sent' && (
                 <div className="mb-4 text-sm font-medium text-green-600">
