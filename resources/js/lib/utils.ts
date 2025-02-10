6fdc1899737cb83239b84f60e184cae43e9b552f
Taylor Otwell <taylor@laravel.com>
2025-02-10 12:35:56 -0600

    remove util

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,11 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
-
-export function cleanupMobileNavigation() {
-    // Remove pointer-events style from body...
-    document.body.style.removeProperty('pointer-events');
-
-    // Dispatch a custom event that the sidebar can listen to...
-    window.dispatchEvent(new CustomEvent('mobile-navigation'));
-}
