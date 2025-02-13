ccff99e86fd8761814d6955ab5043f884a070b7c
Taylor Otwell <taylor@laravel.com>
2025-02-13 17:19:11 -0600

    wip

--- routes/auth.php
+++ routes/auth.php
@@ -1,51 +1,29 @@
 <?php
 
 use App\Models\User;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Laravel\WorkOS\Http\Requests\AuthKitAuthenticationRequest;
 use Laravel\WorkOS\Http\Requests\AuthKitLoginRequest;
 use Laravel\WorkOS\Http\Requests\AuthKitLogoutRequest;
-use WorkOS\UserManagement;
 
 Route::get('login', function (AuthKitLoginRequest $request) {
     return $request->redirect();
 })->middleware(['guest'])->name('login');
 
 Route::get('workos', function (AuthKitAuthenticationRequest $request) {
-    // $response = $request->authenticate();
-
-    $user = (new UserManagement)->authenticateWithCode(
-        config('services.workos.client_id'),
-        $request->query('code'),
+    $user = $request->authenticate(
+        findUsersUsing: function (string $id) {
+            return User::where('workos_id', $id)->first();
+        },
+        createUsersUsing: function ($user) {
+            return User::create([
+                'name' => $user->firstName.' '.$user->lastName,
+                'email' => $user->email,
+                'email_verified_at' => now(),
+                'workos_id' => $user->id,
+            ]);
+        }
     );
-
-    [$user, $accessToken, $refreshToken] = [
-        $user->user,
-        $user->access_token,
-        $user->refresh_token,
-    ];
-
-    $existingUser = User::where('workos_id', $user->id)->first();
-
-    if (! $existingUser) {
-        $existingUser = User::create([
-            'workos_id' => $user->id,
-            'email' => $user->email,
-            'name' => $user->firstName.' '.$user->lastName,
-            'email_verified_at' => now(),
-        ]);
-
-        event(new Registered($existingUser));
-    }
-
-    Auth::login($existingUser);
-
-    $request->session()->put('workos_access_token', $accessToken);
-    $request->session()->put('workos_refresh_token', $refreshToken);
-
-    Session::regenerate();
 
     return redirect('/dashboard');
 });
