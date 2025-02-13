ae661c9341ac4f07e1d0210dbf48e62632d2632c
tnylea <601261+tnylea@users.noreply.github.com>
2025-02-13 21:52:57 +0000

    fix code style

--- resources/js/hooks/use-mobile-navigation.ts
+++ resources/js/hooks/use-mobile-navigation.ts
@@ -4,7 +4,6 @@ export function useMobileNavigation() {
     const cleanup = useCallback(() => {
         // Remove pointer-events style from body...
         document.body.style.removeProperty('pointer-events');
-        
     }, []);
 
     return cleanup;
