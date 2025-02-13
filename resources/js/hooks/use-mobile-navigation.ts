75f6139b93eb64c442b2619fbbda5782e81e6e88
Tony Lea <tony@devdojo.com>
2025-02-13 16:49:43 -0500

    Adding fix for click issue

--- resources/js/hooks/use-mobile-navigation.ts
+++ resources/js/hooks/use-mobile-navigation.ts
@@ -4,13 +4,7 @@ export function useMobileNavigation() {
     const cleanup = useCallback(() => {
         // Remove pointer-events style from body...
         document.body.style.removeProperty('pointer-events');
-
-        // Find and click the close button of any open sheet...
-        const closeButton = document.querySelector('[data-radix-collection-item]');
-
-        if (closeButton instanceof HTMLElement) {
-            closeButton.click();
-        }
+        
     }, []);
 
     return cleanup;
