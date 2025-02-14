56d6e865a6ed65475139f246e1c05e84b3190d0d
tnylea <601261+tnylea@users.noreply.github.com>
2025-02-14 16:14:23 +0000

    fix code style

--- resources/js/layouts/auth/auth-card-layout.tsx
+++ resources/js/layouts/auth/auth-card-layout.tsx
@@ -12,7 +12,7 @@ export default function AuthCardLayout({
     description?: string;
 }) {
     return (
-        <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+        <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="flex w-full max-w-md flex-col gap-6">
                 <a href="#" className="flex items-center gap-2 self-center font-medium">
                     <div className="flex h-10 w-10 items-center justify-center">
