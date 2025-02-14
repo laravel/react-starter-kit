2e75440ca3ba249bd264f368d3fca6a0cdf8037a
Taylor Otwell <taylor@laravel.com>
2025-02-14 16:24:31 -0600

    wip

--- routes/auth.php
+++ routes/auth.php
@@ -1,37 +1,16 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Laravel\WorkOS\Http\Requests\AuthKitAuthenticationRequest;
 use Laravel\WorkOS\Http\Requests\AuthKitLoginRequest;
 use Laravel\WorkOS\Http\Requests\AuthKitLogoutRequest;
-use Laravel\WorkOS\User as UserFromWorkOS;
 
 Route::get('login', function (AuthKitLoginRequest $request) {
     return $request->redirect();
 })->middleware(['guest'])->name('login');
 
 Route::get('authenticate', function (AuthKitAuthenticationRequest $request) {
-    $user = $request->authenticate(
-        findUsing: function (string $id) {
-            return User::where('workos_id', $id)->first();
-        },
-        createUsing: function (UserFromWorkOS $user) {
-            return User::create([
-                'name' => $user->firstName.' '.$user->lastName,
-                'email' => $user->email,
-                'email_verified_at' => now(),
-                'workos_id' => $user->id,
-                'avatar' => $user->avatar ?? '',
-            ]);
-        },
-        updateUsing: function (User $user, UserFromWorkOS $userFromWorkOS) {
-            $user->update([
-                'name' => $userFromWorkOS->firstName.' '.$userFromWorkOS->lastName,
-                'avatar' => $userFromWorkOS->avatar ?? '',
-            ]);
-        },
-    );
+    $user = $request->authenticate();
 
     return redirect('/dashboard');
 });
