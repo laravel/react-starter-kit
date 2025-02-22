48fa72ec67dda6d89c519f173edc8b8d35d9fecf
Tony Lea <tony@devdojo.com>
2025-02-22 09:11:48 -0500

    cleanup

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -2,14 +2,7 @@ import { type SharedData } from '@/types';
 import { Head, Link, usePage } from '@inertiajs/react';
 
 export default function Welcome() {
-    const { auth, laravelVersion, phpVersion } = usePage<SharedData>().props;
-
-    const handleImageError = () => {
-        document.getElementById('screenshot-container')?.classList.add('hidden!');
-        document.getElementById('docs-card')?.classList.add('row-span-1!');
-        document.getElementById('docs-card-content')?.classList.add('flex-row!');
-        document.getElementById('background')?.classList.add('hidden!');
-    };
+    const { auth } = usePage<SharedData>().props;
 
     return (
         <>
