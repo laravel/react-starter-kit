680fb906279da26522b8d268fb84e4ac0dc51e2b
Joel Butcher <joelbutcher@users.noreply.github.com>
2025-02-27 22:56:34 +0000

    Make heading consistent with react starter kit

--- resources/js/components/heading.tsx
+++ resources/js/components/heading.tsx
@@ -1,9 +1,12 @@
+import { Separator } from '@/components/ui/separator';
+
 export default function Heading({ title, description }: { title: string; description?: string }) {
     return (
         <>
             <div className="mb-8 space-y-0.5">
                 <h2 className="text-xl font-semibold tracking-tight">{title}</h2>
                 {description && <p className="text-muted-foreground text-sm">{description}</p>}
+                <Separator className="my-6" />
             </div>
         </>
     );
