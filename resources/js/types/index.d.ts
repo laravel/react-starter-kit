93bc4450510dec26950bff04c9c047db4a3bbe0e
Steve McDougall <JustSteveKing@users.noreply.github.com>
2025-04-14 17:38:59 +0100

    Update index.d.ts
    Adding PaginatedResult interface

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -41,3 +41,27 @@ export interface User {
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
 }
+
+export interface PaginatedResult<T> {
+    data: T[];
+    links: {
+        first: string;
+        last: string;
+        next?: string;
+        prev?: string;
+    };
+    meta: {
+        current_page: number;
+        from?: number;
+        last_page: number;
+        links: {
+            active: boolean;
+            label: string;
+            url?: string;
+        }[];
+        path: string;
+        per_page: number;
+        to?: number;
+        total: number;
+    };
+};
