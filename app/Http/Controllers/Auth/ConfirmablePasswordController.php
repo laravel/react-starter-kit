5dd801931ef0c942375022bc33001cd44f7b4466
David Dorenbos <david_dorenbos@hotmail.com>
2025-06-13 09:06:49 +0200

    Delete unused import

--- app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\ConfirmPasswordRequest;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
