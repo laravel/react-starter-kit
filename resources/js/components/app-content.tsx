dad340240dffb0f511116dc12238987cb2b6830a
Austin Drummond <adrum0x7@gmail.com>
2025-08-30 17:45:21 -0400

    fix content classnames

--- resources/js/components/app-content.tsx
+++ resources/js/components/app-content.tsx
@@ -1,8 +1,14 @@
+import { cn } from '@/lib/utils';
 import * as React from 'react';
 
-export function AppContent({ children, ...props }: React.PropsWithChildren) {
+interface AppContentProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+export function AppContent({ children, className, ...props }: AppContentProps) {
     return (
-        <div className="mx-auto flex h-full w-full max-w-7xl flex-1 flex-col gap-4" {...props}>
+        <div className={cn('mx-auto flex h-full w-full max-w-7xl flex-1 flex-col gap-4', className)} {...props}>
             {children}
         </div>
     );
