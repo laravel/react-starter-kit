4a7d8070c60f3dcad408c5824a2e6223b8ae1eca
Austin Drummond <adrum0x7@gmail.com>
2025-08-30 17:45:15 -0400

    use mantine hook for useismobile

--- resources/js/hooks/use-mobile.tsx
+++ resources/js/hooks/use-mobile.tsx
@@ -1,22 +1,10 @@
-import { useEffect, useState } from 'react';
+import { px } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 
 const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-    const [isMobile, setIsMobile] = useState<boolean>();
-
-    useEffect(() => {
-        const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
-
-        const onChange = () => {
-            setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-        };
-
-        mql.addEventListener('change', onChange);
-        setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-
-        return () => mql.removeEventListener('change', onChange);
-    }, []);
+    const isMobile = useMediaQuery(`(max-width: ${px(MOBILE_BREAKPOINT)})`);
 
     return !!isMobile;
 }
