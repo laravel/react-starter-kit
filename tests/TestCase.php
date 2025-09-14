6c516711e0791ff1473a4206152b8f7f1121a551
Austin Drummond <adrum0x7@gmail.com>
2025-09-13 20:50:51 -0400

    upstream updates

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,9 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use PHPUnit\Framework\Attributes\RequiresEnvironmentVariable;
 
+#[RequiresEnvironmentVariable('APP_BASE_PATH')]
 abstract class TestCase extends BaseTestCase
 {
     //
