2f0f53629f6cc8ca157af8e09199a885147d3ec5
AmroBinMohmmad <Uglyz3tr@gmail.com>
2026-02-09 14:01:05 +0300

    commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <h1>All Todos</h1>
+    <h1>All Todos.</h1>
     <ul class="list-group">
         @foreach($todos as $todo)
             <li class="list-group-item d-flex justify-content-between">
