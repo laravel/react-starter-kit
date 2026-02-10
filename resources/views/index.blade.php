f1b4212e5a69703b486c96cf21159be61f6e293d
AmroBinMohmmad <Uglyz3tr@gmail.com>
2026-02-10 11:27:05 +0300

    modified:   resources/views/index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,14 @@
     <h1>All Todos.</h1>
     <ul class="list-group">
         @foreach($todos as $todo)
+            @if ($todo->updated_at->ne($todo->created_at))
+                <span class="badge bg-secondary">Updated</span>
+            @endif
             <li class="list-group-item d-flex justify-content-between">
                 {{ $todo->name }}
                 <a href="/details/{{ $todo->id }}" class="btn btn-info btn-sm">View</a>
             </li>
         @endforeach
+        
     </ul>
 @endsection
