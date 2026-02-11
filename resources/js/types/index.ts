c8a4233054075aa390569e9bf3bd2efce58c478b
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-02-11 11:25:10 -0500

    Inertia globals (#276)
    Co-authored-by: WendellAdriel <11641518+WendellAdriel@users.noreply.github.com>
    Co-authored-by: Arthur Grand <artygrand@users.noreply.github.com>

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -1,12 +1,3 @@
 export type * from './auth';
 export type * from './navigation';
 export type * from './ui';
-
-import type { Auth } from './auth';
-
-export type SharedData = {
-    name: string;
-    auth: Auth;
-    sidebarOpen: boolean;
-    [key: string]: unknown;
-};
