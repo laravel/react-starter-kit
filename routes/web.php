5169c3447d75eafc92487cfdc22b725e4a14fd13
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-02-25 10:49:36 -0500

    Update workflow to fix issues with the concurrency check (#286)
    Co-authored-by: WendellAdriel <11641518+WendellAdriel@users.noreply.github.com>
    Co-authored-by: WendellAdriel <me@wendelladriel.com>

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 use Laravel\WorkOS\Http\Middleware\ValidateSessionWithWorkOS;
 
-Route::get('/', fn () => Inertia::render('welcome'))->name('home');
+Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware([
     'auth',
     ValidateSessionWithWorkOS::class,
 ])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
 
 require __DIR__.'/settings.php';
