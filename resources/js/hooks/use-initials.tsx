213cd8ea838838658c59588ee4a9efb72eb5a41e
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-03-11 10:41:07 -0400

    ESLint/formatting updates (#311)
    Co-authored-by: WendellAdriel <11641518+WendellAdriel@users.noreply.github.com>
    Co-authored-by: joetannenbaum <2702148+joetannenbaum@users.noreply.github.com>

--- resources/js/hooks/use-initials.tsx
+++ resources/js/hooks/use-initials.tsx
@@ -6,8 +6,13 @@ export function useInitials(): GetInitialsFn {
     return useCallback((fullName: string): string => {
         const names = fullName.trim().split(' ');
 
-        if (names.length === 0) return '';
-        if (names.length === 1) return names[0].charAt(0).toUpperCase();
+        if (names.length === 0) {
+            return '';
+        }
+
+        if (names.length === 1) {
+            return names[0].charAt(0).toUpperCase();
+        }
 
         const firstInitial = names[0].charAt(0);
         const lastInitial = names[names.length - 1].charAt(0);
