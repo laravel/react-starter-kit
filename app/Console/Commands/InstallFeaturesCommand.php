ba70f3646cf80eeee3495c40612d43c590952560
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2026-05-15 06:25:14 -0700

    sync changes from maestro

--- app/Console/Commands/InstallFeaturesCommand.php
+++ app/Console/Commands/InstallFeaturesCommand.php
@@ -29,6 +29,10 @@ class InstallFeaturesCommand extends Command
 
     public function handle(): int
     {
+        if (getenv('LARAVEL_INSTALLER_DEFER_HOOKS') && $this->option('answers') === null) {
+            return self::SUCCESS;
+        }
+
         if (! file_exists(base_path('chisel.php'))) {
             return self::SUCCESS;
         }
@@ -76,9 +80,11 @@ class InstallFeaturesCommand extends Command
 
     protected function buildAssets(): void
     {
+        $npm = Chisel::in(base_path())->npm();
+
         $this->info('Building assets...');
 
-        Chisel::in(base_path())->npm()->run('build');
+        $npm->run('build');
 
         $this->info('Assets built successfully.');
     }
